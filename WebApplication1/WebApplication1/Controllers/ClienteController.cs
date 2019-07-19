6f7f85a5724956eddb3c2e7786731bf760a664cb
cleberspirlandeli <contato.cleberrezende@gmail.com>
2019-07-18 23:47:33 -0300

    projeto crud dotnet core api

---
+++ WebApplication1/WebApplication1/Controllers/ClienteController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private static List<ClienteModels> clientes = new List<ClienteModels>();
+
+        [HttpGet]
+        public List<ClienteModels> Get()
+        {
+            return clientes;
+        }
+
+        [HttpPost]
+        public void Post(string nome)
+        {
+            if (!string.IsNullOrEmpty(nome))
+                clientes.Add(new ClienteModels(nome));
+        }
+
+        [HttpPut]
+        public void Put(string nome)
+        {
+            //clientes.RemoveAt(clientes.IndexOf(clientes.First(x => x.Nome.Equals(nome))));
+            int position = clientes.IndexOf(clientes.First(x => x.Nome.Equals(nome)));
+            Console.WriteLine(position);
+        }
+
+        [HttpDelete]
+        public void Delete(string nome)
+        {
+            clientes.RemoveAt(clientes.IndexOf(clientes.First(x => x.Nome.Equals(nome))));
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/ClienteModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ClienteModels
+    {
+        public ClienteModels(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        public string Nome { get; set; }
+    }
+}
